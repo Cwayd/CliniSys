4564e6ca533d8c52243ecb0c939c5f30f65fd1c7
Cwayd <clydealinsag@gmail.com>
2024-08-30 22:58:01 +0800

    COMMIT

---
+++ Views/Patient/dashboard/dashboard.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        success: '#28a745',
+      },
+    },
+  },
+  plugins: [],
+}
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Views/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
